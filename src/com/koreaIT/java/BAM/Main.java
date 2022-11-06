b88e87c6b50dacb11c90e1ac0325a0261580f2e5
CodeLove02 <CodeLove02@gmail.com>
2022-11-06 10:55:14 +0900

    게시물 리스트 기능 구현중

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,11 +10,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int lastArticleId = 0;
-		
+		List<Article> articles =new ArrayList<>();
 		while(true) {
 			
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if(cmd.length()==0) {
 				System.out.println("명령어를 입력해주세요");
@@ -30,7 +32,7 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				
+				Article article = new Article(id,title,body)
 				
 				System.out.printf("%d글이 생성되었습니다\n",id);
 				
@@ -47,5 +49,17 @@ public class Main {
 		System.out.println("입력된 명령어 : " + cmd);
 		System.out.println("==프로그램 끝==");
 		sc.close();
+	}
+}
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
+				
 	}
 }
