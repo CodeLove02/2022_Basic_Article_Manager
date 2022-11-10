bc73385a5c116853105c5d4e632902b08b69dfc3
CodeLove02 <CodeLove02@gmail.com>
2022-11-10 16:29:07 +0900

    회원가입 기능 구현

--- src/com/koreaIT/java/BAM/App.java
+++ src/com/koreaIT/java/BAM/App.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.koreaIT.java.BAM.dto.Article;
+import com.koreaIT.java.BAM.dto.Member;
 import com.koreaIT.java.BAM.util.Util;
 
 public class App {
 	private List<Article> articles;
+	private List<Member> members;
 
 	App() {
 		articles = new ArrayList<>();
+		members = new ArrayList<>();
 	}
 
 	public void run() {
@@ -51,7 +54,52 @@ public class App {
 				articles.add(article);
 
 				System.out.printf("%s , %s\n", title, body);
-			} else if (cmd.startsWith("article list")) {
+			} else if (cmd.equals("member join")) {
+				int id = articles.size() + 1;
+//				lastArticleId = id;
+				String regDate = Util.getNowDateStr();
+				String loginId = null;
+
+				while (true) {
+					System.out.printf("로그인 아이디 : ");
+					loginId = sc.nextLine();
+
+					if (loginIdChk(loginId) == false) {
+						System.out.printf("%s은(는) 이미 사용중인 아이디입니다\n", loginId);
+						continue;
+					}
+					System.out.printf("%s은(는) 사용가능한 아이디입니다\n",loginId);
+					break;
+				}
+
+				String loginPw = null;
+				String loginPwChk;
+
+				while (true) {
+					System.out.printf("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.printf("비밀번호 확인 : ");
+					loginPwChk = sc.nextLine();
+
+					if (loginPw.equals(loginPwChk) == false) {
+						System.out.println("비밀번호를 다시 입력해주세요");
+						continue;
+					}
+					break;
+				}
+
+				System.out.printf(" 이름 : ");
+				String name = sc.nextLine();
+
+				Member member = new Member(id, regDate, loginId, loginPw, name);
+
+				members.add(member);
+
+				System.out.printf("%s 회원님 환영합니다\n", loginId);
+
+			}
+
+			else if (cmd.startsWith("article list")) {
 
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
@@ -156,6 +204,28 @@ public class App {
 		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
+
+	private boolean loginIdChk(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+		
+	
 
 	private int getArticleIndexById(int id) {
 		int i = 0;
