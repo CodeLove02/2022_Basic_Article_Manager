f85ecd1240590dcf4454140388ab52361ba9db4c
CodeLove02 <CodeLove02@gmail.com>
2022-11-20 02:16:03 +0900

    ArticleService , MemberService 도입

--- src/com/koreaIT/java/BAM/controller/ArticleController.java
+++ src/com/koreaIT/java/BAM/controller/ArticleController.java
@@ -1,22 +1,26 @@
 package com.koreaIT.java.BAM.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import com.koreaIT.java.BAM.container.Container;
 import com.koreaIT.java.BAM.dto.Article;
-import com.koreaIT.java.BAM.dto.Member;
+import com.koreaIT.java.BAM.service.ArticleService;
+import com.koreaIT.java.BAM.service.MemberService;
 import com.koreaIT.java.BAM.util.Util;
 
 public class ArticleController extends Controller {
-	private List<Article> articles;
+
 	private Scanner sc;
 	private String cmd;
+	private ArticleService articleService;
+	private MemberService memberService;
 
 	public ArticleController(Scanner sc) {
-		this.articles = Container.articleDao.articles;
+		this.articleService = Container.articleService;
+		this.memberService = Container.memberService;
 		this.sc = sc;
+		
 
 	}
 
@@ -26,7 +30,6 @@ public class ArticleController extends Controller {
 
 		switch (methodName) {
 		case "write":
-			
 
 			doWrite();
 			break;
@@ -37,7 +40,7 @@ public class ArticleController extends Controller {
 			showDetail();
 			break;
 		case "modify":
-			
+
 			doModify();
 			break;
 		case "delete":
@@ -51,7 +54,7 @@ public class ArticleController extends Controller {
 
 	private void doWrite() {
 
-		int id = Container.articleDao.getNewId();
+		int id = articleService.setArticleId();
 //	lastArticleId = id;
 		String regDate = Util.getNowDateStr();
 		System.out.printf("제목 : ");
@@ -61,52 +64,31 @@ public class ArticleController extends Controller {
 
 		Article article = new Article(id, regDate, loginedMember.id, title, body);
 
-		Container.articleDao.add(article);
+		articleService.add(article);
 
 		System.out.printf("%d 번글이 생성되었습니다\n", id);
 
 	}
 
 	private void showList() {
-		if (articles.size() == 0) {
-			System.out.println("게시물이 없습니다");
-			return;
-		}
-
-		List<Article> forPrintArticles = articles;
 
 		String searchKeyword = cmd.substring("article list".length()).trim();
-		System.out.println("검색어" + searchKeyword);
 
-		if (searchKeyword.length() > 0) {
-			System.out.println("검색어 : " + searchKeyword);
-			forPrintArticles = new ArrayList<>();
-			for (Article article : articles) {
-				if (article.title.contains(searchKeyword)) {
-					forPrintArticles.add(article);
-				}
-			}
-			if (forPrintArticles.size() == 0) {
-				System.out.println("검색결과가 없습니다");
-				return;
-			}
+		List<Article> forPrintArticles = articleService.getForPrintArticles(searchKeyword);
+
+		if (forPrintArticles.size() == 0) {
+			System.out.println("게시물이 없습니다");
+			return;
 		}
 
 		System.out.println("번호    |    제목			| 		날짜	|	작성자				| 		조회");
 		for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
 			Article article = forPrintArticles.get(i);
-			
-			String writerName = null;
-			
-			List<Member> members = Container.memberDao.members;
-			
-			for(Member member : members) {
-				if(article.memberId == member.id) {
-					writerName = member.name;
-					break;
-				}
-			}
-			
+
+			String writerName = memberService.getWriterName(article.memberId);
+
+			// List<Member> members = Container.memberDao.members;
+
 			System.out.printf("%d    |    %s		|     %s 		|   	%s				|			%d \n",
 					article.id, article.title, article.regDate, writerName, article.viewCnt);
 		}
@@ -122,17 +104,21 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(cmdBits[2]);
 
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 
 		if (foundArticle == null) {
 
 			System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 			return;
 		}
+
+		String writerName = Container.memberService.getWriterName(foundArticle.memberId);
+
 		foundArticle.addViewCnt();
+		
 		System.out.printf("번호 : %d\n", foundArticle.id);
 		System.out.printf("날짜 : %s\n", foundArticle.regDate);
-		System.out.printf("작성자 : %s\n", foundArticle.memberId);
+		System.out.printf("작성자 : %s\n", writerName);
 		System.out.printf("제목 : %s\n", foundArticle.title);
 		System.out.printf("내용 : %s\n", foundArticle.body);
 		System.out.printf("조회 : %d\n", foundArticle.viewCnt);
@@ -140,7 +126,7 @@ public class ArticleController extends Controller {
 	}
 
 	private void doModify() {
-		String[] cmdBits = cmd.split("");
+		String[] cmdBits = cmd.split(" ");
 
 		if (cmdBits.length == 2) {
 			System.out.println("article modify 뒤에 번호를 추가해주세요");
@@ -149,7 +135,7 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(cmdBits[2]);
 
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 
 		if (foundArticle == null) {
 
@@ -158,12 +144,12 @@ public class ArticleController extends Controller {
 			return;
 
 		}
-		
-		if(foundArticle.memberId != loginedMember.id) {
+
+		if (foundArticle.memberId != loginedMember.id) {
 			System.out.println("권한이 없습니다");
 			return;
 		}
-		
+
 		System.out.printf("수정할 제목 : ");
 		String title = sc.nextLine();
 		System.out.printf("수정할 내용 : ");
@@ -172,7 +158,7 @@ public class ArticleController extends Controller {
 		foundArticle.title = title;
 		foundArticle.body = body;
 
-		System.out.printf("%d번 게시물이 수정되었습니다", id);
+		System.out.printf("%d번 게시물이 수정되었습니다\n", id);
 	}
 
 	private void doDelete() {
@@ -186,50 +172,29 @@ public class ArticleController extends Controller {
 		int id = Integer.parseInt(cmdBits[2]);
 
 //		int foundIndex = getArticleIndexById(id);
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 
 		if (foundArticle == null) {
 
 			System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 			return;
 		}
-		
-		if(foundArticle.memberId != loginedMember.id) {
+
+		if (foundArticle.memberId != loginedMember.id) {
 			System.out.println("권한이 없습니다");
 			return;
 		}
+		articleService.remove(foundArticle);
 
-		articles.remove(foundArticle);
 		System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
 
 	}
 
-	private int getArticleIndexById(int id) {
-		int i = 0;
-		for (Article article : articles) {
-			if (article.id == id) {
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-
-	private Article getArticleById(int id) {
-
-		int index = getArticleIndexById(id);
-
-		if (index != -1) {
-			return articles.get(index);
-		}
-		return null;
-	}
-
 	public void makeTestData() {
 		System.out.println("테스트를 위한 게시물을 생성합니다");
-		Container.articleDao.add(new Article(Container.articleDao.getNewId(), Util.getNowDateStr(), 1, "제목1", "내용1", 11));
-		Container.articleDao.add(new Article(Container.articleDao.getNewId(), Util.getNowDateStr(), 2, "제목2", "내용2", 22));
-		Container.articleDao.add(new Article(Container.articleDao.getNewId(), Util.getNowDateStr(), 2, "제목3", "내용3", 33));
+		articleService.add(new Article(articleService.setArticleId(), Util.getNowDateStr(), 1, "제목1", "내용1", 11));
+		articleService.add(new Article(articleService.setArticleId(), Util.getNowDateStr(), 2, "제목2", "내용2", 22));
+		articleService.add(new Article(articleService.setArticleId(), Util.getNowDateStr(), 2, "제목3", "내용3", 33));
 	}
 
 }
--- src/com/koreaIT/java/BAM/controller/MemberController.java
+++ src/com/koreaIT/java/BAM/controller/MemberController.java
@@ -1,28 +1,25 @@
 package com.koreaIT.java.BAM.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import com.koreaIT.java.BAM.container.Container;
-import com.koreaIT.java.BAM.dto.Article;
 import com.koreaIT.java.BAM.dto.Member;
+import com.koreaIT.java.BAM.service.MemberService;
 import com.koreaIT.java.BAM.util.Util;
 
 public class MemberController extends Controller {
-	private List<Member> members;
+	
 	private Scanner sc;
-	
-	
+	private MemberService memberService;
+
 
 	public MemberController(Scanner sc) {
-		this.members = Container.memberDao.members;
+		this.memberService = Container.memberService;
 		this.sc = sc;
 	}
 
 	@Override
 	public void doAction(String cmd, String methodName) {
-		
 
 		switch (methodName) {
 		case "join":
@@ -37,56 +34,71 @@ public class MemberController extends Controller {
 		case "logout":
 			doLogout();
 			break;
-		default :
+		default:
 			System.out.println("존재하지 않는 명령어입니다");
 			break;
 		}
 		
 	}
 
-
 	private void doLogout() {
-		
-		
+
 		loginedMember = null;
 		System.out.println("로그아웃 되었습니다");
-		
+
 	}
 
 	private void showProfile() {
-		
-		
+
 		System.out.println("==내 정보==");
-		System.out.printf("로그인 아이디 : %s\n" , loginedMember.loginId);
-		System.out.printf("이름 : %s\n" , loginedMember.name);
+		System.out.printf("로그인 아이디 : %s\n", loginedMember.loginId);
+		System.out.printf("이름 : %s\n", loginedMember.name);
 	}
 
 	private void doLogin() {
-		
-		
-		
-		System.out.printf("로그인 아이디 : ");
-		String loginId = sc.nextLine();
-		System.out.printf("로그인 비밀번호 : ");
-		String loginPw = sc.nextLine();
-		
-		Member member = getMemberByLoginId(loginId);
-		
-		if(member == null){
-			System.out.println("일치하는 아이디가 없습니다");
-			}
-		if(member.loginPw.equals(loginPw) == false) {
-			System.out.println("비밀번호가 일치하지 않습니다");
-			return;
-		}
-			loginedMember = member;
-			System.out.printf("로그인 성공! %s님 환영합니다\n",loginedMember.name);
-		}
+		Member member = null;
+		String loginPw = null;
+		while (true) {
 
+			System.out.printf("로그인 아이디 : ");
+			String loginId = sc.nextLine();
+
+			if (loginId.trim().length() == 0) {
+				System.out.println("로그인 아이디를 입력해주세요");
+				continue;
+			}
+
+			while (true) {
+
+				System.out.printf("로그인 비밀번호 : ");
+				loginPw = sc.nextLine();
+
+				if (loginPw.trim().length() == 0) {
+					System.out.println("비밀번호를 입력해주세요");
+					continue;
+				}
+				break;
+			}
+
+			member = memberService.getMemberByLoginId(loginId);
+
+			if (member == null) {
+				System.out.println("일치하는 아이디가 없습니다");
+				return;
+			}
+			if (member.loginPw.equals(loginPw) == false) {
+				System.out.println("비밀번호가 일치하지 않습니다");
+				return;
+			}
+			break;
+		}
+		loginedMember = member;
+		System.out.printf("로그인 성공! %s님 환영합니다\n", loginedMember.name);
+	}
 
 	private void doJoin() {
 
-		int id = Container.memberDao.getNewId();
+		int id = memberService.setArticleId();
 //		lastArticleId = id;
 		String regDate = Util.getNowDateStr();
 		String loginId = null;
@@ -95,7 +107,7 @@ public class MemberController extends Controller {
 			System.out.printf("로그인 아이디 : ");
 			loginId = sc.nextLine();
 
-			if (loginIdChk(loginId) == false) {
+			if (memberService.loginIdChk(loginId) == false) {
 				System.out.printf("%s은(는) 이미 사용중인 아이디입니다\n", loginId);
 				continue;
 			}
@@ -124,48 +136,18 @@ public class MemberController extends Controller {
 
 		Member member = new Member(id, regDate, loginId, loginPw, name);
 
-		Container.memberDao.add(member);
+		memberService.add(member);
 
 		System.out.printf("%s 회원님 환영합니다\n", loginId);
 	}
 
 	
-	
-	
-	private boolean loginIdChk(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
 
-		if (index == -1) {
-			return true;
-		}
-		return false;
-	}
-
-	private int getMemberIndexByLoginId(String loginId) {
-		int i = 0;
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-
-	private Member getMemberByLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
-		
-		if(index != -1) {
-			return members.get(index);
-		}
-		
-		return null;
-	}
-	
 	public void makeTestData() {
 		System.out.println("테스트를 위한 회원을 생성합니다");
-		Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "test1", "test1", "김철수"));
-		members.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "test2", "test2", "김영희"));
-		members.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "test3", "test3", "박영수"));
+		
+		memberService.add(new Member(memberService.setArticleId(), Util.getNowDateStr(), "test1", "test1", "김철수"));
+		memberService.add(new Member(memberService.setArticleId(), Util.getNowDateStr(), "test2", "test2", "김영희"));
+		memberService.add(new Member(memberService.setArticleId(), Util.getNowDateStr(), "test3", "test3", "박영수"));
 	}
 }
